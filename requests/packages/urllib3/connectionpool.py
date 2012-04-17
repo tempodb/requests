98661fca6ef44b8e5c58a54d8825a6d5e6fe5192
Mike Yagley <myagley@gmail.com>
2012-04-17 15:25:04 -0500

    Default to an SSLv3 socket and fallback to SSLv23 on failure

--- requests/packages/urllib3/connectionpool.py
+++ requests/packages/urllib3/connectionpool.py
@@ -95,9 +95,19 @@ class VerifiedHTTPSConnection(HTTPSConnection):
 
         # Wrap socket using verification with the root certs in
         # trusted_root_certs
-        self.sock = ssl.wrap_socket(sock, self.key_file, self.cert_file,
-                                    cert_reqs=self.cert_reqs,
-                                    ca_certs=self.ca_certs)
+        # Revert to SSL v23 if the SSLv3 socket fails.
+        # See: http://bugs.python.org/issue11220
+        try:
+            self.sock = ssl.wrap_socket(sock, self.key_file, self.cert_file,
+                                        cert_reqs=self.cert_reqs,
+                                        ca_certs=self.ca_certs,
+                                        ssl_version=ssl.PROTOCOL_SSLv3)
+        except ssl.SSLError:
+            self.sock = ssl.wrap_socket(sock, self.key_file, self.cert_file,
+                            cert_reqs=self.cert_reqs,
+                            ca_certs=self.ca_certs,
+                            ssl_version=ssl.PROTOCOL_SSLv23)
+
         if self.ca_certs:
             match_hostname(self.sock.getpeercert(), self.host)
 
